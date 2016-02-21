3346e737515a7d7ab4dc6eba7f55d3ba4d5018ea
Logan Patino <loganpatino10@gmail.com>
2016-02-21 12:40:45 -0500

    Change style to not use default action bar

--- TheBeast/app/src/main/java/com/thebeast/com/thebeast/HomeActivity.java
+++ TheBeast/app/src/main/java/com/thebeast/com/thebeast/HomeActivity.java
@@ -2,12 +2,18 @@ package com.thebeast.com.thebeast;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
 
     @Override
@@ -15,7 +21,25 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer_layout);
 
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
 }
