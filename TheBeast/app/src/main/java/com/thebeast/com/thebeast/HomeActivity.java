0ffe59ef178ead04757f2968e43ee3614b26c758
Logan Patino <loganpatino10@gmail.com>
2016-02-25 00:56:34 -0500

    Implement click listeners for navigation menu items

--- TheBeast/app/src/main/java/com/thebeast/com/thebeast/HomeActivity.java
+++ TheBeast/app/src/main/java/com/thebeast/com/thebeast/HomeActivity.java
@@ -2,6 +2,8 @@ package com.thebeast.com.thebeast;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -36,6 +38,9 @@ public class HomeActivity extends AppCompatActivity {
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nvView);
+        setUpNavigationView(navigationView);
+
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.content_frame, new TabFragment());
@@ -55,6 +60,9 @@ public class HomeActivity extends AppCompatActivity {
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 break;
+            case R.id.nav_first_fragment:
+                Log.d("NAV TEST", "First item clicked");
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -63,5 +71,47 @@ public class HomeActivity extends AppCompatActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         mDrawerToggle.syncState();
+    }
+
+    private void setUpNavigationView(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                selectItem(item);
+                return true;
+            }
+        });
+    }
+
+    public void selectItem(MenuItem item) {
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        switch (item.getItemId()) {
+            case R.id.nav_first_fragment:
+                fragmentClass = TabFragment.class;
+                break;
+            case R.id.nav_second_fragment:
+                fragmentClass = TestFragment2.class;
+                break;
+            case R.id.nav_third_fragment:
+                fragmentClass = TestFragment3.class;
+                break;
+            default:
+                fragmentClass = TabFragment.class;
+        }
+
+        try {
+            fragment = (Fragment)fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame, fragment);
+        fragmentTransaction.commit();
+
+        item.setChecked(true);
+        mDrawerLayout.closeDrawers();
     }
 }
